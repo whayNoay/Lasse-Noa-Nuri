b7b465b65144adc1adcb6ea0f73d36b36bedc4e1
whayNoay <noakordt@gmail.com>
2025-05-19 12:22:48 +0200

    Update Noa.py

--- Noa.py
+++ Noa.py
@@ -5,22 +5,39 @@ def mülltonnen_info():
 
     return f"Mülltonne an Koordinaten {x_koordinate}x {y_koordinate}y in der Stadt {stadt}"
 
+
+def mülltonnenart():
+    typ = input("Welchen Typ hat die Mülltonne?(Papier,Restmüll,Bio oder Plastik): ")
+    
+    return f" Der Typ der Mülltonne ist{typ}"
+
+
 def füllstand():
     return int(input("Gib den Füllstand der Mülltonne in Prozent ein: "))  
 
+
+   
+
+
 def prüfe_füllstand(füllstand):
     if füllstand < 30:
-        return "Leer  :)"
+        return "Leer und muss aktuell nicht abgeholt werden :)"
     elif 30 <= füllstand <= 69:
-        return "Mittelvoll  :|"
+        return "Mittelvoll und sollte in nächster Zeit abgeholt werden :|"
     else:
-        return "Voll  :("
+        return "Voll und muss dringend abgeholt werden :("
 
 
 print(mülltonnen_info())  
 
-füllstand_wert = füllstand()  
-print(f"Der Füllstand der Mülltonne ist {prüfe_füllstand(füllstand_wert)} ")
+print(mülltonnenart())
+
+mülltonnenart_wert = typ
+füllstand_wert = füllstand()
+
+print(f"Der Füllstand der {typ}mülltonne ist {prüfe_füllstand(füllstand_wert)} ")
+
+
 
 
 

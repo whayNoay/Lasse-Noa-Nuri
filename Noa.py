1fc77dbdbe6c8378e0cdc92f7478f991502134d0
whayNoay <noakordt@gmail.com>
2025-05-26 12:19:38 +0200

    Update Noa.py

--- Noa.py
+++ Noa.py
@@ -1,36 +1,36 @@
 
-x_koordinate = input("Gib die X-Koordinate der Mülltonne ein: ")
-y_koordinate = input("Gib die Y-Koordinate der Mülltonne ein: ")
-stadt = input("Gib die Stadt an in der die Mülltonne steht: ")
-strasse = input("Gib deine Straße an: ")
+x_koordinate = input("Gib die X-Koordinate des Mülltonnes ein: ")
+y_koordinate = input("Gib die Y-Koordinate des Mülleimers ein: ")
+stadt = input("Gib die Stadt an in der, der Mülleimer steht: ")
+strasse = input("Gib die Straße an in der, der Mülleimer steht: ")
 hausnummer = input("Gib dein Hausnummer an:")
 
 fuellstand = int(input("Gib den Füllstand der Mülltonne in cm ein: "))  
 
 if fuellstand < 30 :
-    status = ("  Die Mülltonne ist Leer und muss aktuell nicht abgeholt werden :)")
+    status = ("  Der Mülleimer ist Leer und muss aktuell nicht abgeholt werden :)")
 elif 30 <= fuellstand <= 69:
-    status = ("Die Mülltonne ist Mittelvoll und sollte in nächster Zeit abgeholt werden :|")
+    status = ("Der Mülleimer ist Mittelvoll und sollte in nächster Zeit abgeholt werden :|")
 else:
-    status = ("Die Mülltonne ist Voll und muss dringend abgeholt werden :(")
+    status = ("Der Mülleimer ist Voll und muss dringend abgeholt werden :(")
     
 
-fuellstand_cm = fuellstand / 1.5
+fuellstand_prozent = fuellstand / 1.5
 
-muelltonnenart =input("Welche Art hat die Muelltonnen(Papier,Plastik,Bio oder Restmüll)?:")
+muelltonnenart =input("Welche Art hat der Mülleimer(Papier,Plastik,Bio oder Restmüll)?:")
 
-ausgabe2 = f"Die {muelltonnenart}-Mülltonne steht in {stadt} in der Straße {strasse} bei der Hausnummer {hausnummer} und hat die Koordinaten {x_koordinate} x {y_koordinate} y mit dem Füllstand von {fuellstand}cm welches umgerechnet {fuellstand_cm}% sind.{status}"
+
+ausgabe2 = f"Der {muelltonnenart}-Mülleimer steht in {stadt} in der Straße {strasse} bei der Hausnummer {hausnummer} und hat die Koordinaten {x_koordinate} x {y_koordinate} y mit dem Füllstand von {fuellstand}cm welches umgerechnet {fuellstand_prozent}% sind.{status}"
 print(ausgabe2)
 
 
-eingabe = input("Schreibe 'Senden' wenn du deine endgültigen Daten an die App schicken möchtest, um deine Mülltonne zu registrieren: ")
+eingabe = input("Schreibe 'Senden' wenn die endgültigen Daten an die App geschickt werden sollen, um den Mülleimer zu registrieren: ")
 
 if eingabe.lower() == "senden":      #zeile mit KI gemacht https://copilot.microsoft.com/chats/1wdhbpr2TwPnaJC6YMdbH
-    print("Daten wurden erfolgreich gesendet")
+    print("Daten wurden erfolgreich gesendet ")
 else:
-    print("Registrierung abgebrochen. Drücken Sie erneut das grüne Startsymbol oben links am Bildschirm, um Ihre Daten zu ändern.")
+    print("Registrierung abgebrochen. Grünes Startsymbol muss oben links am Bildschirm gedrückt werden, um die Daten zu ändern.")
 
         
         
         
-#zeile mit KI gemacht https://copilot.microsoft.com/chats/1wdhbpr2TwPnaJC6YMdbH
